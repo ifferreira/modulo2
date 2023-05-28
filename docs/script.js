ca3d1a781be15edf3219dd5d2de44855844ef579
ifferreira <ivan.ferreira@sou.inteli.edu.br>
2023-05-27 23:15:57 -0300

    etapa 6 curriculo

---
+++ docs/script.js
@@ -0,0 +1,5 @@
+$("#imgPerfil").on({
+    click:()=>{$("#nome").css("color", "red")},
+    mouseenter:()=>{$("body").css("background-color", "purple")},
+    mouseleave:()=>{$("body").css("background-color", "white")}
+})
