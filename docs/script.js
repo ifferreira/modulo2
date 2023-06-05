246fa41f1e42e007f3680d7ccbeaecbc91eda23f
ifferreira <ivan.ferreira@sou.inteli.edu.br>
2023-06-04 23:20:02 -0300

    etapa 7

--- docs/script.js
+++ docs/script.js
@@ -3,3 +3,13 @@ $("#imgPerfil").on({
     mouseenter:()=>{$("body").css("background-color", "purple")},
     mouseleave:()=>{$("body").css("background-color", "white")}
 })
+
+$("#btnPerfil").click(()=>{
+    let ajaxTeste = new XMLHttpRequest();
+    ajaxTeste.onreadystatechange = ()=>{
+        document.getElementById("nome").innerHTML = this.responseText;
+    }
+
+    ajaxTeste.open("GET", "caminhoDaImg.txt", true);
+    ajaxTeste.send();
+})
